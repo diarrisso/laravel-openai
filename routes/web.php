3ddcd7a00c05347fd2ba527fa3af6dd63058fd63
Diarrisso <diarrisso49@gmail.com>
2025-05-05 16:20:53 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
+use OpenAI\Laravel\Facades\OpenAI;
 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/openai', function() {
+
+    $result =  OpenAI::chat()->create([
+        'model' => 'gpt-4o-mini',
+        'messages' => [
+            ['role' => 'user', 'content' => 'Hello!'],
+        ],
+    ]);
+
+    echo $result->choices[0]->message->content;
 });
